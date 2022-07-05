78bbd6b653ac830fa444f99acbaa0dc28221aa37
ugurcatak <ugurcatak@gmail.com>
2022-07-05 11:41:42 +0300

    fix

--- js-pages/checkout.js
+++ js-pages/checkout.js
@@ -1179,8 +1179,6 @@ function createAddressBubble(section, source) {
     $('#selected-' + section + '-address').html(content);
 	$('#selected-' + section + '-address').show();	
 	$('#' + section + '-country').change();	
-	$("input[name='shipping-address2']").val('');
-	$("input[name='billing-address2']").val('');
 	
 	if($("input[name='shipping-same-billing']").is(':checked') && section == 'shipping')
 		$("input[name='shipping-same-billing']").change();
